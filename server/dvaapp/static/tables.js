9ba1812b217e20bb874b293b733c24e7c479a79a
sunxingba <1928462315@qq.com>
2018-01-17 11:39:43 +0800

    modify language url value

--- server/dvaapp/static/tables.js
+++ server/dvaapp/static/tables.js
@@ -1,17 +1,17 @@
 function InitializeTables(){
     $('.dataTables').dataTable({
         responsive: true,
-		language:{url: "chinese.json"}   
+		language:{url: "/chinese.json"}   
     });
     $('.dataTables-dict').dataTable({
         responsive: true,
         "bPaginate": false,
-		language:{url: "chinese.json"}   
+		language:{url: "/chinese.json"}   
     });
     $('.dataTables-nofilter').dataTable({
         responsive: true,
         "bPaginate": false,
         "bFilter": false,
-		language:{url: "chinese.json"} 
+		language:{url: "/chinese.json"} 
     });
 }
