8ee3549808e66dcf5af0fa68ceefcbd2a9e78f2b
sunxingba <1928462315@qq.com>
2018-01-17 11:02:32 +0800

    add file Chinese.json Used to set Language option for table to Chinese

--- server/dvaapp/static/tables.js
+++ server/dvaapp/static/tables.js
@@ -1,17 +1,17 @@
 function InitializeTables(){
     $('.dataTables').dataTable({
         responsive: true,
-		language:{url: "Chinese.json"}   
+		language:{url: "chinese.json"}   
     });
     $('.dataTables-dict').dataTable({
         responsive: true,
         "bPaginate": false,
-		language:{url: "Chinese.json"}   
+		language:{url: "chinese.json"}   
     });
     $('.dataTables-nofilter').dataTable({
         responsive: true,
         "bPaginate": false,
         "bFilter": false,
-		language:{url: "Chinese.json"} 
+		language:{url: "chinese.json"} 
     });
 }
