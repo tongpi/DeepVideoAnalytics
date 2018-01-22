8efb011274ee8260ac0b76c76d557af05685698f
sunxingba <1928462315@qq.com>
2018-01-22 12:09:06 +0800

    update 'test error 'in views.py

--- server/dvaui/views.py
+++ server/dvaui/views.py
@@ -982,6 +982,7 @@ def deprecate_current_app(func):
     """
     @functools.wraps(func)
     def inner(*args, **kwargs):
+	print " here errored test2 "
         if 'current_app' in kwargs:
             warnings.warn(
                 "Passing `current_app` as a keyword argument is deprecated. "
@@ -1001,6 +1002,7 @@ def logout(request, next_page=None,
            template_name='registration/logged_out.html',
            redirect_field_name=REDIRECT_FIELD_NAME,
            extra_context=None):
+	print " here errored test1 "
     warnings.warn(
         'The logout() view is superseded by the class-based LogoutView().',
         RemovedInDjango21Warning, stacklevel=2
