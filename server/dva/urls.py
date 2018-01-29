6f1a6caa8f1c47d8f16051c15fa28618327730fb
sunxingba <1928462315@qq.com>
2018-01-29 16:28:25 +0800

    test admin login

--- server/dva/urls.py
+++ server/dva/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.contrib.auth import views as auth_views
 admin.autodiscover()
 
-urlpatterns = [url(r'^admin/', admin.site.urls),
+urlpatterns = [url(r'^login/$', auth_views.login, name='login'),
+			   url(r'^admin/', admin.site.urls),
                url(r'^api/', include('dvaapp.urls')),
                url(r'', include('dvaui.urls'))]+\
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
