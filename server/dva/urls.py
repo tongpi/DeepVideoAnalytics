d17c86a0de3ab930e4dd97d0e8f42da22ed498e5
sunxingba <1928462315@qq.com>
2018-01-29 17:24:28 +0800

    test admin login

--- server/dva/urls.py
+++ server/dva/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from ..dvaui import urls as as_urls
+from .dvaui import urls as as_urls
 
 admin.autodiscover()
 
