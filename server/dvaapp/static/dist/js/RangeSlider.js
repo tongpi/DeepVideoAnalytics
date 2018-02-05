cbea89621775edaafe99d765bc73afe18d46892e
sunxingba <1928462315@qq.com>
2018-02-05 13:24:55 +0800

    modify dashboard.html css

--- server/dvaapp/static/dist/js/RangeSlider.js
+++ server/dvaapp/static/dist/js/RangeSlider.js
@@ -18,7 +18,7 @@ $.fn.RangeSlider = function(cfg){
 
     $input.bind("input", function(e){
         $input.attr('value', this.value);
-        $input.css( 'background', 'linear-gradient(to right, #565e6b, #565e6b' + this.value + '%, #c9c7c7)' );
+        $input.css( 'background', 'linear-gradient(to right, #565e6b, #565e6b' + this.value + '%, #565e6b)' );
 
         if ($.isFunction(callback)) {
             callback(this);
